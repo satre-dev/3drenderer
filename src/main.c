61217908975fff5dbbeedafaaab910638b27422e
SamTredgett <sam.tredgett@gmail.com>
2024-06-11 23:46:23 +0100

    implemented loading meshes from a .obj file

--- src/main.c
+++ src/main.c
@@ -37,7 +37,8 @@ void setup(void) {
     );
 
     // loads the cube values in the mesh data structure
-    load_cube_mesh_data();
+    // load_cube_mesh_data();
+    load_obj_file_data("./assets/f22.obj");
 }
 
 void process_input(void) {
--- src/mesh.c
+++ src/mesh.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "mesh.h"
 #include "array.h"
 
@@ -46,4 +48,44 @@ void load_cube_mesh_data(void) {
 
     for (int i =0; i < N_CUBE_FACES; i++) 
         array_push(mesh.faces, cube_faces[i]);
+}
+
+void load_obj_file_data(char* filename) {
+    FILE* file;
+    file = fopen(filename, "r");
+    char line[1024];
+
+    while (fgets(line, 1024, file)) {
+        // Vertex info
+        if (strncmp(line, "v ", 2) == 0) {
+            vec3_t vertex;
+            sscanf(line, "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
+            array_push(mesh.vertices, vertex);
+        }
+
+        // Face information
+        if(strncmp(line, "f ", 2) == 0) {
+            int vertex_indices[3];
+            int texture_indices[3];
+            int normal_indices[3];
+            sscanf(line, "f %d/%d/%d %d/%d/%d %d/%d/%d", 
+                    &vertex_indices[0],
+                    &texture_indices[0],
+                    &normal_indices[0],
+                    &vertex_indices[1],
+                    &texture_indices[1],
+                    &normal_indices[1],
+                    &vertex_indices[2],
+                    &texture_indices[2],
+                    &normal_indices[2]
+            );
+
+            face_t face = {
+                .a = vertex_indices[0],
+                .b = vertex_indices[1],
+                .c = vertex_indices[2],
+            };
+            array_push(mesh.faces, face);
+        }
+    }
 }
