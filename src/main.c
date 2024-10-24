11951b3e65b6c02293f2794a6f13e4987d4dd18c
dabi-ngin <sam.tredgett@gmail.com>
2024-10-24 22:40:14 +0100

    Added backface culling.

--- src/main.c
+++ src/main.c
@@ -13,8 +13,8 @@
 triangle_t* triangles_to_render = NULL;
 
 // Global variables for execution status and game loop
-vec3_t camera_position = {.x =0,.y = 0,.z = -5};
-float fov_factor = 640;
+vec3_t camera_position = {0, 0, 0};
+float fov_factor = 540;
 
 bool is_running = false;
 int previous_frame_time = 0;
@@ -36,21 +36,7 @@ void setup(void) {
         window_height
     );
 
-    // loads the cube values in the mesh data structure
-    // load_cube_mesh_data();
     load_obj_file_data("./assets/f22.obj");
-    vec3_t a = {
-	    .x = 0.2,
-	    .y = 1.3,
-	    .z = 7.1
-      };
-    vec3_t b = {
-	    .x = 8.2,
-	    .y = 3.2,
-	    .z = 2.2
-      };
-    vec3_t result = vec3_sub(a, b);
-    printf("result= %f,%f,%f", result.x, result.y, result.z);
 }
 
 void process_input(void) {
@@ -102,18 +88,50 @@ void update(void) {
         face_vertices[1] = mesh.vertices[mesh_face.b - 1];
         face_vertices[2] = mesh.vertices[mesh_face.c - 1];
 
-        triangle_t projected_triangle;
+        vec3_t transformed_vertices[3];
+
         // Loop all 3 vertices of this current face and apply transformations
         for (int j = 0; j < 3; j++) {
             vec3_t transformed_vertex = face_vertices[j];
             transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
             transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
             transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
+
             // Translate the vertex away from the camera
-            transformed_vertex.z -= camera_position.z;
+            transformed_vertex.z += 5;
+
+            // Save transformed vertex in array of transformed vertices
+            transformed_vertices[j] = transformed_vertex;
+        }
+
+        // loop all the three vertices to perform projection
+        vec3_t vector_a  = transformed_vertices[0];
+        vec3_t vector_b  = transformed_vertices[1];
+        vec3_t vector_c  = transformed_vertices[2];
+
+
+        // Get the vector subtraction B-A and C-A
+        vec3_t vector_ab = vec3_sub(vector_b, vector_a);
+        vec3_t vector_ac = vec3_sub(vector_c, vector_a);
+
+        // Compute the face normal (using cross product to find perpendicular)
+        vec3_t normal = vec3_cross(vector_ab, vector_ac);
+
+        // Find the ray between camera and a point in the triangle
+        vec3_t camera_ray = vec3_sub(camera_position, vector_a);
+
+        // Calculate camera ray alignment with face normal
+        float dot_normal_camera = vec3_dot(normal, camera_ray);
+
+        if (dot_normal_camera < 0) {
+            continue;
+        }
+
+        triangle_t projected_triangle;
+        for (int j = 0; j < 3; j++){
 
             // project the current vertex
-            vec2_t projected_point = project(transformed_vertex);
+            vec2_t projected_point = project(transformed_vertices[j]);
 
             // Scale and translate the projected point to the middle of the screen
             projected_point.x += (window_width / 2);
--- src/vector.c
+++ src/vector.c
@@ -19,6 +19,26 @@ vec2_t vec2_sub(vec2_t a, vec2_t b) {
 	return result;
 }
 
+vec2_t vec2_mul(vec2_t v, float factor){
+    vec2_t result = {
+        .x = v.x * factor,
+        .y = v.y * factor
+    };
+    return result;
+}
+
+vec2_t vec2_div(vec2_t v, float factor){
+    vec2_t result = {
+        .x = v.x / factor,
+        .y = v.y / factor
+    };
+    return result;
+}
+
+float vec2_dot(vec2_t a, vec2_t b) {
+    return (a.x *b.x) + (a.y * b.y);
+}
+
 //////////////////////////////////////////////////
 // Implementations of Vector 3D functions
 //////////////////////////////////////////////////
@@ -27,12 +47,8 @@ float vec3_length(vec3_t v) {
 }
 
 vec3_t vec3_add(vec3_t a, vec3_t b) {
-        vec3_t result = {
-          .x = a.x + b.x,
-          .y = a.y + b.y,
-	  .z = a.z + b.z
-        };
-	return result;
+        vec3_t result = {.x = a.x + b.x, .y = a.y + b.y, .z = a.z + b.z};
+        return result;
 }
 
 vec3_t vec3_sub(vec3_t a, vec3_t b) {
@@ -42,6 +58,37 @@ vec3_t vec3_sub(vec3_t a, vec3_t b) {
           .z = a.z - b.z
         };
 	return result;
+}
+
+vec3_t vec3_mul(vec3_t v, float factor){
+    vec3_t result = {
+        .x = v.x * factor,
+        .y = v.y * factor,
+        .z = v.z * factor
+    };
+    return result;
+}
+
+vec3_t vec3_div(vec3_t v, float factor){
+    vec3_t result = {
+        .x = v.x / factor,
+        .y = v.y / factor,
+        .z = v.z / factor
+    };
+    return result;
+}
+
+vec3_t vec3_cross(vec3_t a, vec3_t b) {
+    vec3_t result = {
+        .x = a.y * b.z - a.z * b.y,
+        .y = a.z * b.x - a.x * b.z,
+        .z = a.x * b.y - a.y * b.x
+    };
+    return result;
+}
+
+float vec3_dot(vec3_t a, vec3_t b) {
+    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
 }
 
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
